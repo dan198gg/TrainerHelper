680bd5ea13d9ce1b9caea0bf289f56721b042be0
dan198gg <danzelgoncharenko@gmail.com>
2024-03-31 21:33:46 +0300

    RecyclerViewIsUpdated

--- app/src/main/java/com/example/trainerhelper/CustomAdapter.kt
+++ app/src/main/java/com/example/trainerhelper/CustomAdapter.kt
@@ -1,19 +1,24 @@
 package com.example.trainerhelper
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class CustomAdapter(var context: Context,
                     var muscules:ArrayList<String>
                     ): RecyclerView.Adapter<CustomAdapter.MyViewHolder>() {
     inner class MyViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
-        var muscule: Button
+        var muscule: TextView
+        var musculeRow:ConstraintLayout
         init{
             muscule=itemView.findViewById(R.id.buttonMuscule)
+            musculeRow=itemView.findViewById(R.id.musculeRow)
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -24,9 +29,14 @@ class CustomAdapter(var context: Context,
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.muscule.text=muscules[position]
+        holder.musculeRow.setOnClickListener {
+            var intent1=Intent(context,TrainActivity::class.java)
+            context.startActivity(intent1)
+        }
     }
 
     override fun getItemCount(): Int {
         return muscules.size
     }
+
 }
---
+++ app/src/main/java/com/example/trainerhelper/TrainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trainerhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_train)
+    }
+}
